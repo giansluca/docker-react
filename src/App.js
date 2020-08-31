520b0af41c487877719bc7bd8a16579d4f2a333d
gians <giansluca@gmail.com>
2020-09-01 00:17:44 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - Gians ...
+          Learn React - Gians Dev.
         </a>
       </header>
     </div>
